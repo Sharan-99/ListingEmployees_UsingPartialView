9732bfac22e4ba732b922744946c95be7e762c6f
Sharan-99 <sharan.ceg2017103591@gmail.com>
2022-05-25 11:33:46 +0530

    first commit

---
+++ ListingEmployees_UsingPartialView/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using ListingEmployees_UsingPartialView.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListingEmployees_UsingPartialView.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            List<Employee> emp = new List<Employee>()
+            {
+                new Employee() {Id=1,Name="John",Salary=10000,IsPermanent=true},
+                new Employee() {Id=2,Name="Smith",Salary=5000,IsPermanent=false},
+                new Employee() {Id=3,Name="Mark",Salary=5000,IsPermanent=false},
+                new Employee() {Id=4,Name="Mary",Salary=5000,IsPermanent=false}
+
+            };
+            return View(emp);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult GetEmployees()
+        {
+            List<Employee> emp = new List<Employee>()
+            {
+                new Employee() {Id=1,Name="John",Salary=10000,IsPermanent=true},
+                new Employee() {Id=2,Name="Smith",Salary=5000,IsPermanent=false},
+                new Employee() {Id=3,Name="Mark",Salary=5000,IsPermanent=false},
+                new Employee() {Id=4,Name="Mary",Salary=5000,IsPermanent=false}
+
+            };
+            return View(emp);
+        }
+    }
+}
